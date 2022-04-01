3b0112e9f3be0962b81e5b71391135dd0585adc7
Oleg <oa.demin1305@omgau.org>
2022-04-02 02:55:51 +0700

    fix: PrimeNumber

--- src/main/java/hexlet/code/Games/PrimeNumber.java
+++ src/main/java/hexlet/code/Games/PrimeNumber.java
@@ -17,8 +17,8 @@ public class PrimeNumber {
         String[][] roundsData = new String[LIMIT_LENGTH_ARRAYS][2];
         for (int i = 0; i < LIMIT_LENGTH_ARRAYS; i++) {
             int number = Utils.generateOfNumber(MAX_RANDOM) + 1;
-            var question = Primes.isPrime(number) ? "yes" : "no";
-            var answer = String.valueOf(number);
+            var answer = Primes.isPrime(number) ? "yes" : "no";
+            var question = String.valueOf(number);
             roundsData[i] = new String[]{question, answer};
         }
         return roundsData;
